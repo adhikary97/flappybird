2d2755e37ef1f5dac27ffcd7b506b53dab180b20
Paras Adhikary <adhikary@purdue.edu>
2018-10-29 11:34:30 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
+<php echo "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>";>
 <?php include_once("index.html")?>
-<php echo "<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>\n";>
